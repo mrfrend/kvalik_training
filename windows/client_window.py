5e4c7e7b018502c4d8eea28a9051f60ece7607be
Ivan <ideshchenko@yandex.ru>
2025-12-21 16:35:52 +0300

    add filter by status client

--- windows/client_window.py
+++ windows/client_window.py
@@ -1,7 +1,7 @@
 from PyQt6.QtCore import QDate
 from interfaces.client import Ui_Form
 from PyQt6.QtWidgets import QMessageBox, QWidget, QApplication, QTableWidgetItem
-from database.db import get_available_rooms
+from database.db import get_available_rooms, get_categories
 
 class ClientWindow(Ui_Form, QWidget): 
     def __init__(self):
@@ -11,6 +11,13 @@ class ClientWindow(Ui_Form, QWidget):
         self.filter_button.clicked.connect(self.load_rooms)
         self.check_in_date.setMinimumDate(QDate.currentDate())
         self.check_out_date.setMinimumDate(QDate.currentDate())
+        self.load_categories()
+    
+    def load_categories(self):
+        categories = get_categories()
+        for category in categories:
+            self.category_name_box.addItem(category["category_name"], category["id"])
+        
     
     def load_rooms(self):
         check_in_date = self.check_in_date.date()
@@ -23,7 +30,8 @@ class ClientWindow(Ui_Form, QWidget):
         check_in_date = check_in_date.toString("yyyy-MM-dd")
         check_out_date = check_out_date.toString("yyyy-MM-dd")
 
-        data = get_available_rooms(check_in_date, check_out_date)
+        data = get_available_rooms(check_in_date, check_out_date) # Фильтрация в БД по дате
+        data = [room for room in data if room["category_name"] == self.category_name_box.currentText()]
 
         if len(data) == 0:
             QMessageBox.information(self, "Бронирования", "Свободных номеров на данный период нет")
